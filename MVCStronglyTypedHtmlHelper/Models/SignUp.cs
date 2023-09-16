efbc8b2932409cf537c8c4691554e249e40b1755
Tusher Ahmed <ahmedshad1997@gmail.com>
2023-09-16 23:26:30 +0600

    Add project files.

---
+++ MVCStronglyTypedHtmlHelper/Models/SignUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCStronglyTypedHtmlHelper.Models
+{
+    public class SignUp
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string UserPassword { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string UserComment { get; set; }
+    }
+}
